c5de9cdb1f7e5d3cd90a6f4d7953c555667c9b65
EFEKAN <150671591+xefekqnnn@users.noreply.github.com>
2024-12-15 12:05:42 +0100

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,11 +1,11 @@
-let map = L.map('wilrijkmap').setView([51.1572, 4.4028], 15); // Coördinaten van Wilrijk
+let map = L.map('wilrijkmap').setView([51.1572, 4.4028], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 30,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let circle = L.circle([51.1572, 4.4028], { // Coördinaten voor de cirkel in Wilrijk
+let circle = L.circle([51.1572, 4.4028], { 
     color: 'blue',
     fillColor: '#3A8DF4',
     fillOpacity: 0.5,
